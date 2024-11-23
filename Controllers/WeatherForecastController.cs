4f4bc368173aae252f9e19e3665539cf478461e8
s.kiani <sa@7913882>
2024-11-23 19:10:06 +0330

    add first event

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,32 +1,32 @@
+using Marten;
 using Microsoft.AspNetCore.Mvc;
 
 namespace event_sourcing.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class TestController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+    private readonly ILogger<TestController> _logger;
+    private readonly IDocumentSession _session;
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public TestController(ILogger<TestController> logger, IDocumentSession session)
     {
         _logger = logger;
+        _session = session;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    [HttpPost]
+    public async Task Post()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        var @event = new PromissoryCreated(Guid.NewGuid(), new Random().NextInt64().ToString(), DateTime.Now, Guid.NewGuid(), Guid.NewGuid());
+        _session.Events.StartStream(@event.Id, @event);
+        await _session.SaveChangesAsync();
     }
 }
+
+// public record BaseEvent(Guid Id, DateTime CreatedOn, Guid CreatedBy, Guid RowVersion);
+
+public sealed record PromissoryCreated(Guid Id, string promissoryId, DateTime CreatedOn, Guid CreatedBy, Guid RowVersion);
+
+
